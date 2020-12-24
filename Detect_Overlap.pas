66a0c88bf9fe64769b5d3a6f4d2ba997ae616923
Stephen Thompson <Stephen.Revel.Thompson@gmail.com>
2020-12-24 11:45:13 -0500

    Removed unused code

--- Detect_Overlap.pas
+++ Detect_Overlap.pas
@@ -103,316 +103,11 @@ begin
     T2 := Rect2.Top;
     B2 := Rect2.Bottom;
 
-    // Overlap in X direction
-    //if ((L > L2) and (L < R2)) or ((L2 > L) and (L2 < R)) or ((R < R2) and (R > L2)) or ((R2 < R) and (R2 > L)) then
-    //begin
-    //    OverX := True;
-    //end;
-    // Overlap in Y direction
-    //if ((B > B2) and (B < T2)) or ((B2 > B) and (B2 < T)) or ((T < T2) and (T > B2)) or ((T2 < T) and (T2 > B)) then
-    //begin
-    //    OverY := True;
-    //end;
-    // Must Overlap in both directions for true overlap
-    //if OverX and OverY then
-    //begin
-    //    result := True; Exit; // Equivalent to return in C
-    //end;
-
     if (B > T2) or (T < B2) or (L > R2) or (R < L2) then
     begin
         result := False; Exit; // Equivalent to return in C
     end;
     result := True;
-end;
-
-// Get pads for surrounding area
-function GetSilkAroundComponent(Board: IPCB_Board, Dataset: TStringList, Cmp: IPCB_Component, Filter_Size: Integer): TStringList;
-const
-    DEL = ','; // CSV Delimeter
-var
-    Iterator      : IPCB_SpatialIterator;
-    RectL         : TCoord;
-    RectR         : TCoord;
-    RectB         : TCoord;
-    RectT         : TCoord;
-    Cmp_Layer     : TPCBString;
-    Cmp_x, Cmp_y  : Integer;
-    Cmp_RefDes, Slk_RefDes    : TPCBString;
-    Slk_x, Slk_y  : Integer;
-    Slk           : IPCB_Text;
-    SlkIsDes      : Boolean;
-    Slk_Layer     : TPCBString;
-    Slk_Rot       : TPCBString;
-    Slk_Rect      : TCoordRect;
-    Slk_L         : TPCBString;
-    Slk_R         : TPCBString;
-    Slk_T         : TPCBString;
-    Slk_B         : TPCBString;
-    Slk_H         : Boolean;
-    Slk_IsDes     : Boolean;
-    delta_x       : Integer;
-    delta_y       : Integer;
-    delta         : TPCBString;
-    xorigin, yorigin : Integer;
-begin
-    RectL := Cmp.x - Filter_Size; // Rectangle Left Filter Starting Point
-    RectR := Cmp.x + Filter_Size; // Rectangle Right Filter Stopping Point
-    RectB := Cmp.y - Filter_Size; // Rectangle Bottom Filter Starting Point
-    RectT := Cmp.y + Filter_Size; // Rectangle Top Filter Stopping Point
-
-    Cmp_Layer := Layer2String(Cmp.Layer);
-
-    Iterator := Board.SpatialIterator_Create;
-    Iterator.AddFilter_ObjectSet(MkSet(eTextObject));
-    If Cmp_Layer = 'Top Layer' Then
-    Begin
-        Iterator.AddFilter_LayerSet(MkSet(eTopOverlay));
-    End
-    Else
-    Begin
-        Iterator.AddFilter_LayerSet(MkSet(eBottomOverlay));
-    End;
-    Iterator.AddFilter_Area(RectL, RectB, RectR, RectT);
-
-    xorigin := Board.XOrigin;
-    yorigin := Board.YOrigin;
-
-    Slk := Iterator.FirstPCBObject;
-    While Slk <> NIL Do
-    Begin
-        SlkIsDes := Slk.IsDesignator;
-        // Skip silk for current component. Only get surrounding silkscreen
-        If (Cmp.Name.Text = Slk.Text) Or Not(SlkIsDes) Then
-        Begin
-            Slk := Iterator.NextPCBObject;
-            Continue;
-        End;
-
-        If (Slk.IsHidden) Then
-        Begin
-            Slk := Iterator.NextPCBObject;
-            Continue;
-        End;
-
-        Slk_Layer := Layer2String(Slk.Layer);
-        Slk_Rot := FloatToStr(Slk.Rotation);
-        Slk_Rect := Slk.BoundingRectangle;
-        Slk_L := IntToStr(Slk_Rect.Left - xorigin);
-        Slk_R := IntToStr(Slk_Rect.Right - xorigin);
-        Slk_T := IntToStr(Slk_Rect.Top - yorigin);
-        Slk_B := IntToStr(Slk_Rect.Bottom - yorigin);
-
-        Cmp_x := Cmp.x;
-        Cmp_y := Cmp.y;
-        Slk_x := Slk.XLocation;
-        Slk_y := Slk.YLocation;
-        delta_x := abs(Cmp.x - Slk.XLocation);
-        delta_y := abs(Cmp.y - Slk.YLocation);
-        delta := FloatToStr(sqrt(sqr(delta_x) + sqr(delta_y)));
-
-
-        Dataset.Add(Cmp.Identifier+DEL+Slk.Component.Identifier+DEL+Slk_L+DEL+Slk_R+DEL+Slk_T+DEL+Slk_B+DEL+Slk_Rot+DEL+Slk_Layer+DEL+delta);
-
-        Slk := Iterator.NextPCBObject;
-    End;
-
-    Client.SendMessage('PCB:RunQuery', 'Clear=True' , 255, Client.CurrentView);
-    Board.BoardIterator_Destroy(Iterator);
-
-    result := Dataset;
-end;
-
-function PadShapeToStr(iteration : Integer): TPCBString;
-begin
-  Case iteration of
-       eNoShape : result := 'No_Shape';
-       eRounded : result := 'Rounded';
-       eRectangular : result := 'Rectangular';
-       eOctagonal : result := 'Octagonal';
-       eCircleShape : result := 'Circle_Shape';
-       eArcShape : result := 'Arc_Shape';
-       eTerminator : result := 'Terminator';
-       eRoundRectShape : result := 'Round_Rectangular_Shape';
-       eRotatedRectShape : result := 'Rotated_Rectangular_Shape';
-       eRoundedRectangular : result := 'Rounded_Rectangular';
-  else     result := 'Unkown';
-  end;
-end;
-
-// Get pads for surrounding area
-function GetPadsAroundComponent(Board: IPCB_Board, Dataset: TStringList, Cmp: IPCB_Component, Filter_Size: Integer): TStringList;
-const
-    DEL = ','; // CSV Delimeter
-var
-    Iterator      : IPCB_SpatialIterator;
-    RectL         : Integer;
-    RectR         : Integer;
-    RectB         : Integer;
-    RectT         : Integer;
-    Cmp_Layer     : TPCBString;
-    Pad           : IPCB_Primitive;
-    Pad_Layer     : TPCBString;
-    Pad_x         : TPCBString;
-    Pad_y         : TPCBString;
-    Pad_x_dim     : TPCBString;
-    Pad_y_dim     : TPCBString;
-    Pad_h         : TPCBString;
-    Pad_w         : TPCBString;
-    Pad_Rotation  : TPCBString;
-    Pad_Rect      : TCoordRect;
-    Pad_L         : TPCBString;
-    Pad_R         : TPCBString;
-    Pad_T         : TPCBString;
-    Pad_B         : TPCBString;
-    Shape         : Integer;
-    XSize,YSize   : TPCBString;
-    CornerRad     : TPCBString;
-    X1,Y1,X2,Y2   : TCoord;
-    delta_x       : Integer;
-    delta_y       : Integer;
-    delta         : TPCBString;
-    xorigin, yorigin : Integer;
-begin
-    RectL := Cmp.x - Filter_Size; // Rectangle Left Filter Starting Point
-    RectR := Cmp.x + Filter_Size; // Rectangle Right Filter Stopping Point
-    RectB := Cmp.y - Filter_Size; // Rectangle Bottom Filter Starting Point
-    RectT := Cmp.y + Filter_Size; // Rectangle Top Filter Stopping Point
-
-    Cmp_Layer := Layer2String(Cmp.Layer);
-
-    Iterator := Board.SpatialIterator_Create;
-    Iterator.AddFilter_ObjectSet(MkSet(ePadObject));
-    Iterator.AddFilter_LayerSet(MkSet(eTopLayer,eBottomLayer,eMultiLayer));
-    Iterator.AddFilter_Area(RectL, RectB, RectR, RectT);
-
-    xorigin := Board.XOrigin;
-    yorigin := Board.YOrigin;
-
-    // Search for component body objects and get their Name, Kind, Area and OverallHeight values
-    Pad := Iterator.FirstPCBObject;
-    While (Pad <> Nil) Do
-    Begin
-        Pad_x := IntToStr(Pad.x - xorigin);
-        Pad_y := IntToStr(Pad.y - yorigin);
-        Pad_h := IntToStr(Pad.TopXSize);
-        Pad_w := IntToStr(Pad.TopYSize);
-        Pad_Layer := Layer2String(Pad.Layer);
-        Pad_Rotation := FloatToStr(Pad.Rotation);
-        Pad_Rect := Pad.BoundingRectangle;
-        Pad_L := IntToStr(Pad_Rect.Left - xorigin);
-        Pad_R := IntToStr(Pad_Rect.Right - xorigin);
-        Pad_T := IntToStr(Pad_Rect.Top - yorigin);
-        Pad_B := IntToStr(Pad_Rect.Bottom - yorigin);
-        Shape := Pad.TopShape; // eOctagonal, eRectangular, eRounded
-        CornerRad := IntToStr(Pad.CRPercentage[Pad.Layer]);
-        XSize := IntToStr(Pad.TopXSize);
-        YSize := IntToStr(Pad.TopYSize);
-
-        delta_x := abs(Cmp.x - Pad.x);
-        delta_y := abs(Cmp.y - Pad.y);
-        delta := FloatToStr(sqrt(sqr(delta_x) + sqr(delta_y)));
-
-        If Cmp_Layer = 'Bottom Layer' Then
-        Begin
-            Pad_h := IntToStr(Pad.BotXSize);
-            Pad_w := IntToStr(Pad.BotYSize);
-        End;
-
-        If (Pad_Layer = Cmp_Layer) or (Pad_Layer = 'Multi Layer') Then
-        Begin
-            Dataset.Add(Cmp.Identifier+DEL+Pad_x+DEL+Pad_y+DEL+Pad_L+DEL+Pad_R+DEL+Pad_T+DEL+Pad_B+DEL+
-            Pad_Rotation+DEL+PadShapeToStr(Shape)+DEL+CornerRad+DEL+XSize+DEL+YSize+DEL+Cmp_Layer+DEL+delta);
-        End;
-
-        Pad := Iterator.NextPCBObject;
-    End;
-
-    Board.BoardIterator_Destroy(Iterator);
-
-    result := Dataset;
-end;
-
-// Get tracks for surrounding area
-function GetTracksAroundComponent(Board: IPCB_Board, Dataset: TStringList, Cmp: IPCB_Component, Filter_Size: Integer): TStringList;
-const
-    DEL = ','; // CSV Delimeter
-var
-    Iterator      : IPCB_SpatialIterator;
-    RectL         : TCoord;
-    RectR         : TCoord;
-    RectB         : TCoord;
-    RectT         : TCoord;
-    Cmp_Layer     : TPCBString;
-    Cmp_x, Cmp_y  : Integer;
-    Cmp_RefDes, Slk_RefDes    : TPCBString;
-    Slk_x, Slk_y  : Integer;
-    Track         : IPCB_Track;
-    Trk_Layer     : TPCBString;
-    Trk_Rect      : TCoordRect;
-    Trk_L         : TPCBString;
-    Trk_R         : TPCBString;
-    Trk_T         : TPCBString;
-    Trk_B         : TPCBString;
-    Trk_X1, Trk_X2, Trk_Y1, Trk_Y2 : TPCBString;
-    Trk_W         : TPCBString;
-    Trk_H         : Boolean;
-    delta_x       : Integer;
-    delta_y       : Integer;
-    delta         : TPCBString;
-    xorigin, yorigin : Integer;
-begin
-    RectL := Cmp.x - Filter_Size; // Rectangle Left Filter Starting Point
-    RectR := Cmp.x + Filter_Size; // Rectangle Right Filter Stopping Point
-    RectB := Cmp.y - Filter_Size; // Rectangle Bottom Filter Starting Point
-    RectT := Cmp.y + Filter_Size; // Rectangle Top Filter Stopping Point
-
-    Cmp_Layer := Layer2String(Cmp.Layer);
-    Cmp_RefDes := Cmp.Identifier;
-
-    Iterator := Board.SpatialIterator_Create;
-    Iterator.AddFilter_ObjectSet(MkSet(eTrackObject));
-    If Cmp_Layer = 'Top Layer' Then
-    Begin
-        Iterator.AddFilter_LayerSet(MkSet(eTopOverlay));
-    End
-    Else
-    Begin
-        Iterator.AddFilter_LayerSet(MkSet(eBottomOverlay));
-    End;
-    Iterator.AddFilter_Area(RectL, RectB, RectR, RectT);
-
-    xorigin := Board.XOrigin;
-    yorigin := Board.YOrigin;
-
-    Track := Iterator.FirstPCBObject;
-    While Track <> NIL Do
-    Begin
-        Trk_Layer := Layer2String(Track.Layer);
-        Trk_X1 := IntToStr(Track.x1 - xorigin);
-        Trk_X2 := IntToStr(Track.x2 - xorigin);
-        Trk_Y1 := IntToStr(Track.y1 - yorigin);
-        Trk_Y2 := IntToStr(Track.y2 - yorigin);
-        Trk_W := IntToStr(Track.Width);
-
-        Cmp_x := Cmp.x;
-        Cmp_y := Cmp.y;
-        delta_x := abs(Cmp.x - Track.x1);
-        delta_y := abs(Cmp.y - Track.y1);
-        delta := FloatToStr(sqrt(sqr(delta_x) + sqr(delta_y)));
-
-        // Get closer of 2 deltas
-        delta_x := abs(Cmp.x - Track.x2);
-        delta_y := abs(Cmp.y - Track.y2);
-        If FloatToStr(sqrt(sqr(delta_x) + sqr(delta_y))) < delta Then
-            delta := FloatToStr(sqrt(sqr(delta_x) + sqr(delta_y)));
-
-        Dataset.Add(Cmp_RefDes+DEL+Trk_X1+DEL+Trk_X2+DEL+Trk_Y1+DEL+Trk_Y2+DEL+Trk_W+DEL+Trk_Layer+DEL+delta);
-
-        Track := Iterator.NextPCBObject;
-    End;
-    Board.BoardIterator_Destroy(Iterator);
-    result := Dataset;
 end;
 
 // Get components for surrounding area
@@ -450,11 +145,11 @@ begin
         begin
             if Layer2String(Slk.Layer) = 'Top Overlay' then
             begin
-                 Iterator.AddFilter_LayerSet(MkSet(eTopLayer, eMechanical3));
+                 Iterator.AddFilter_LayerSet(MkSet(eTopLayer, eMechanical3, eMultiLayer));
             end
             else
             begin
-                 Iterator.AddFilter_LayerSet(MkSet(eBottomLayer, eMechanical3));
+                 Iterator.AddFilter_LayerSet(MkSet(eBottomLayer, eMechanical3, eMultiLayer));
             end;
         end
         else
@@ -511,27 +206,6 @@ begin
     result := False;
 end;
 
-
-
-function AutoSave_wDial(Count: Integer, autosave_cnt: Integer, Dataset: TStringList, Slk_Data: TStringList, Pad_Data: TStringList, Trk_Data: TStringList, Cmp_Data: TStringList): Boolean;
-var
-   btnChoice : Integer;
-begin
-    result := False;
-    If Count mod autosave_cnt = autosave_cnt-1 Then
-    Begin
-         btnChoice := messagedlg('Continue Collecting Data?', mtCustom, mbYesNoCancel, 0);
-         If btnChoice = mrNo Then
-            result := True;
-
-         Dataset.SaveToFile(MACRODIR+CSVFILE);
-         Slk_Data.SaveToFile(MACRODIR+SLKCSVFILE);
-         Pad_Data.SaveToFile(MACRODIR+PADCSVFILE);
-         Trk_Data.SaveToFile(MACRODIR+TRKCSVFILE);
-         Cmp_Data.SaveToFile(MACRODIR+CMPCSVFILE);
-    End;
-end;
-
 function AutoPosToStr(iteration : Integer): TPCBString;
 begin
   Case iteration of
@@ -546,74 +220,6 @@ begin
        eAutoPos_Manual : result := 'Manual';
   else     result := 'Unkown';
   end;
-end;
-
-function Add_Cmp_Dataset_Row(Dataset:TStringList, Cmp:IPCB_Component, SlkGood: TPCBString, AutoPos:Integer, xorigin: Integer, yorigin: Integer, Filt_Size: Integer);
-const
-   DEL = ','; // CSV Delimeter
-var
-   Silkscreen    : IPCB_Text;
-   CmpName       : TPCBString;
-   CmpNameOn     : TPCBString;
-   CmpXCoord,CmpYCoord : TPCBString;
-   Cmp_Rect,Slk_Rect : TCoordRect;
-   Cmp_L,Cmp_R,Cmp_T,Cmp_B : TPCBString;
-   CmpRot        : TPCBString;
-   SilkXCoord,SilkYCoord : TPCBString;
-   Slk_L,Slk_R,Slk_T,Slk_B : TPCBString;
-   SilkH         : Integer;
-   SilkW         : Integer;
-   SilkX,NewSilkX : Integer;
-   SilkY,NewSilkY : Integer;
-   SilkCentX     : Integer;
-   SilkCentY     : Integer;
-   SilkXStr      : TPCBString;
-   SilkYStr      : TPCBString;
-   SilkInv       : TPCBString;
-   SilkRot       : TPCBString;
-   SilkJust      : TPCBString;
-   SilkHidden    : TPCBString;
-   SilkMir       : TPCBString;
-begin
-   CmpName := Cmp.Identifier;
-   CmpXCoord := IntToStr(Cmp.x - xorigin);
-   CmpYCoord := IntToStr(Cmp.y - yorigin);
-   CmpRot := IntToStr(Cmp.Rotation);
-   Cmp_Rect := Cmp.BoundingRectangleNoNameComment;
-   Cmp_L := IntToStr(Cmp_Rect.Left - xorigin);
-   Cmp_R := IntToStr(Cmp_Rect.Right - xorigin);
-   Cmp_T := IntToStr(Cmp_Rect.Top - yorigin);
-   Cmp_B := IntToStr(Cmp_Rect.Bottom - yorigin);
-
-   Silkscreen := Cmp.Name;
-   SilkH := Silkscreen.Size;
-   SilkW := Silkscreen.Width;
-   SilkXCoord := IntToStr(Silkscreen.XLocation - xorigin);
-   SilkYCoord := IntToStr(Silkscreen.YLocation - yorigin);
-   Slk_Rect := Silkscreen.BoundingRectangle;
-   Slk_L := IntToStr(Slk_Rect.Left - xorigin);
-   Slk_R := IntToStr(Slk_Rect.Right - xorigin);
-   Slk_T := IntToStr(Slk_Rect.Top - yorigin);
-   Slk_B := IntToStr(Slk_Rect.Bottom - yorigin);
-   SilkInv := BoolToStr(Silkscreen.Inverted);
-   SilkRot := FloatToStr(Silkscreen.Rotation);
-   SilkJust := IntToStr(Silkscreen.TTFInvertedTextJustify);
-   SilkMir := BoolToStr(Silkscreen.MirrorFlag);
-   SilkHidden := BoolToStr(Silkscreen.IsHidden);
-
-   Dataset.Add(CmpName+DEL+Layer2String(Cmp.Layer)+DEL+CmpRot+DEL+Cmp_L+DEL+Cmp_R+DEL+Cmp_T+DEL+Cmp_B+DEL+SlkGood+DEL+AutoPosToStr(AutoPos)
-             +DEL+SilkHidden+DEL+SilkInv+DEL+SilkRot+DEL+SilkJust+DEL+SilkMir+DEL+CmpXCoord+DEL+CmpYCoord+DEL+SilkXCoord
-             +DEL+SilkYCoord+DEL+Slk_L+DEL+Slk_R+DEL+Slk_T+DEL+Slk_B+DEL+IntToStr(Silkscreen.Size)+DEL+IntToStr(Silkscreen.Width)
-             +DEL+IntToStr(Filt_Size));
-end;
-
-function CharCount(const S: string): integer;
-var
-  i: Integer;
-begin
-  result := 0;
-  for i := 1 to Length(S) do
-    inc(result);
 end;
 
 function GetBestCmpFilterSize(Cmp: IPCB_Component, Filter_Size: Integer): Integer;
@@ -790,8 +396,6 @@ Begin
         SilkX := Silkscreen.XLocation;
         SilkY := Silkscreen.YLocation;
 
-        //IsOver := Is_Overlapping(Silkscreen, Cmp);
-
         // Skip hidden silkscreen
         If Silkscreen.IsHidden Then
         Begin
@@ -800,9 +404,9 @@ Begin
         End;
 
         // Set visible layers
-        //Board.LayerIsDisplayed[Cmp.Layer] := True;
-        //Board.LayerIsDisplayed[Silkscreen.Layer] := True;
-        //PrevLayer := SetVisibleLayerSideUp(Cmp.Layer, PrevLayer);
+        Board.LayerIsDisplayed[Cmp.Layer] := True;
+        Board.LayerIsDisplayed[Silkscreen.Layer] := True;
+        PrevLayer := SetVisibleLayerSideUp(Cmp.Layer, PrevLayer);
 
         BestFilterSize := GetBestCmpFilterSize(Cmp, FilterSize);
 
